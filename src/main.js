72380c5e1776f46e8d63796425aa8079fb91f0aa
Roma Romanenko <romanenko.roma92@gmail.com>
2022-08-12 20:27:10 +0300

    done HW_11

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix';
+import axios from 'axios';
 
 let lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
@@ -10,7 +11,7 @@ let lightbox = new SimpleLightbox('.gallery a', {
 });
 let caunt = 1;
 let clear = '';
-
+let cauntPages = 40;
 const refs = {
   searchForm: document.querySelector('.search-form'),
   submitBtn: document.querySelector('.submit'),
@@ -24,35 +25,71 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '28934161-1d72d18718d0b61d346597ac7';
 const PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true';
 
-/* const getPost = async () => {
-  const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${refs.searchForm.searchQuery.value}&${PARAMS}&per_page=40&page=${caunt}`
-  );
-  const data = await response.json();
-  refs.searchForm.searchQuery.value = '';
-  console.log(refs.searchForm.searchQuery.value);
-  if (data.totalHits === 0) {
+const getPost = async e => {
+  try {
+    clear = refs.searchForm.searchQuery.value;
     loadBtnOff();
-    Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  } else {
-    loadBtnOn();
-    Notify.success(`Hoooray! We found ${data.totalHits} images!`);
-  }
+    e.preventDefault();
+    reset();
+    caunt = 1;
+    cauntPages = 40;
 
-  caunt += 1;
-  console.log(data);
-  console.log(data.hits);
-  renderMarkup(data.hits);
-  addSearch();
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${refs.searchForm.searchQuery.value}&${PARAMS}&per_page=40&page=${caunt}`
+    );
+    refs.searchForm.searchQuery.value = '';
+    if (response.data.totalHits === 0) {
+      loadBtnOff();
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      loadBtnOn();
+      Notify.success(`Hoooray! We found ${response.data.totalHits} images!`);
+    }
+    renderMarkup(response.data.hits);
+    refs.searchList.insertAdjacentHTML(
+      'beforeend',
+      renderMarkup(response.data.hits)
+    );
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const post = getPost();
-console.log(post);
+const getMore = async e => {
+  try {
+    caunt += 1;
+    cauntPages += 40;
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${clear}&${PARAMS}&per_page=40&page=${caunt}`
+    );
 
-const renderMarkup = post => {
-  return post
+    caunt += 1;
+    renderMarkup(response.data.hits);
+    refs.searchList.insertAdjacentHTML(
+      'beforeend',
+      renderMarkup(response.data.hits)
+    );
+    lightbox.refresh();
+
+    if (cauntPages > response.data.totalHits) {
+      loadBtnOff();
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+refs.submitBtn.addEventListener('click', getPost);
+refs.getBtn.addEventListener('click', getMore);
+
+function renderMarkup(data) {
+  return data
     .map(
       photo =>
         `<div class="photo-card">
@@ -79,103 +116,7 @@ const renderMarkup = post => {
           `
     )
     .join('');
-};
-
-console.log(renderMarkup);
-
-const addSearch = searchMarkup => {
-  refs.searchList.insertAdjacentHTML('beforeend', searchMarkup.markup);
-  lightbox.refresh();
-}; */
-
-const getPost = e => {
-  clear = refs.searchForm.searchQuery.value;
-  loadBtnOff();
-  reset();
-  caunt = 1;
-  e.preventDefault();
-  fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${refs.searchForm.searchQuery.value}&${PARAMS}&per_page=40&page=${caunt}`
-  )
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      refs.searchForm.searchQuery.value = '';
-      console.log(refs.searchForm.searchQuery.value);
-      if (data.totalHits === 0) {
-        loadBtnOff();
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        loadBtnOn();
-        Notify.success(`Hoooray! We found ${data.totalHits} images!`);
-      }
-
-      caunt += 1;
-      console.log(data.totalHits);
-      console.log(data.hits);
-      return data.hits;
-    })
-    .then(renderMarkup)
-    .then(addSearch)
-    .catch(error => console.log(error));
-};
-
-const getMore = e => {
-  fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${clear}&${PARAMS}&per_page=40&page=${caunt}`
-  )
-    .then(response => response.json())
-    .then(data => {
-      caunt += 1;
-      return data.hits;
-    })
-    .then(renderMarkup)
-    .then(addSearch)
-    .catch(error => console.log(error));
-};
-
-refs.submitBtn.addEventListener('click', getPost);
-refs.getBtn.addEventListener('click', getMore);
-
-const renderMarkup = array => {
-  return {
-    markup: array
-      .map(
-        photo =>
-          `<div class="photo-card">
-          
-           <a class="gallery__item" href="${photo.largeImageURL}">
-            <img class="gallery__image" src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy"  width="300" height="200" />
-            </a>
-             <div class="info">
-              <p class="info-item">
-                <b>Likes </br>${photo.likes}</b>
-              </p>
-              <p class="info-item">
-                <b>Views </br>${photo.views}</b>
-              </p>
-              <p class="info-item">
-                <b>Comments  </br>${photo.comments} </b>
-              </p>
-              <p class="info-item">
-                <b>Downloads  </br>${photo.downloads}</b>
-              </p>
-            </div>
-           
-          </div>
-          `
-      )
-      .join(''),
-  };
-};
-
-const addSearch = searchMarkup => {
-  refs.searchList.insertAdjacentHTML('beforeend', searchMarkup.markup);
-  lightbox.refresh();
-};
+}
 
 const reset = () => {
   refs.searchList.innerHTML = '';
